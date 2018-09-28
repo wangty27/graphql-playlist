de016fbf9332dfbed9b559a4dcd9e94119cbb2e7
Tianyu Wang <terrywang@tianyu.ninja>
2018-09-28 17:15:30 -0400

    Client + AddBook + BookList

---
+++ client/src/components/BookList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books {
+      id
+      name
+    }
+  }
+`;
+
+class BookList extends Component {
+  displayBooks() {
+    let data = this.props.data;
+    if(data.loading){
+      return ( <div>Loading books...</div> );
+    } else {
+      return data.books.map(book => {
+        return (
+          <li key={book.id}>{ book.name }</li>
+        );
+      });
+    }
+  }
+  render() {
+    return (
+      <div id="main">
+        <ul id="book-list">
+          { this.displayBooks() }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const schema = require('./schema/schema');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb://localhost/graphql-playlist', { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
 })
 
-app.use('/', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
